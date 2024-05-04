fb46a52a53d693996f263f1a81dfa1eb028d00d1
jspmern <jspmern@gmail.com>
2024-05-04 18:06:36 +0530

    mock data

---
+++ mockdata.js
@@ -0,0 +1,122 @@
+ [
+    {
+        name: "Restaurant 1",
+        stars: 4,
+        score: 80,
+        cuisine: ["American", "Indian", "Japanese", "Chinese"]
+    },
+    {
+        name: "Restaurant 2",
+        stars: 3,
+        score: 70,
+        cuisine: ["Japanese", "Chinese", "American", "Indian"]
+    },
+    {
+        name: "Restaurant 3",
+        stars: 5,
+        score: 90,
+        cuisine: ["Chinese", "Japanese", "Indian", "American"]
+    },
+    {
+        name: "Restaurant 4",
+        stars: 4,
+        score: 80,
+        cuisine: ["Indian", "American", "Chinese", "Japanese"]
+    },
+    {
+        name: "Restaurant 5",
+        stars: 3,
+        score: 80,
+        cuisine: ["Japanese", "Chinese", "Indian", "American"]
+    },
+    {
+        name: "Restaurant 6",
+        stars: 4,
+        score: 80,
+        cuisine: ["Chinese", "American", "Japanese", "Indian"]
+    },
+    {
+        name: "Restaurant 7",
+        stars: 4,
+        score: 90,
+        cuisine: ["Indian", "Chinese", "American", "Japanese"]
+    },
+    {
+        name: "Restaurant 8",
+        stars: 5,
+        score: 100,
+        cuisine: ["American", "Japanese", "Chinese", "Indian"]
+    },
+    {
+        name: "Restaurant 9",
+        stars: 3,
+        score: 70,
+        cuisine: ["Chinese", "Indian", "Japanese"]
+    },
+    {
+        name: "Restaurant 10",
+        stars: 4,
+        score: 90,
+        cuisine: ["Japanese", "American", "Chinese", "Indian"]
+    },
+    {
+        name: "Restaurant 11",
+        stars: 4,
+        score: 20,
+        cuisine: ["Chinese", "Japanese", "Indian"]
+    },
+    {
+        name: "Restaurant 12",
+        stars: 3,
+        score: 80,
+        cuisine: ["Japanese", "Chinese", "Indian"]
+    },
+    {
+        name: "Restaurant 13",
+        stars: 5,
+        score: 90,
+        cuisine: ["Chinese", "Japanese", "Indian"]
+    },
+    {
+        name: "Restaurant 14",
+        stars: 4,
+        score: 80,
+        cuisine: ["Indian", "Chinese", "Japanese"]
+    },
+    {
+        name: "Restaurant 15",
+        stars: 3,
+        score: 90,
+        cuisine: ["Japanese", "Chinese", "Indian"]
+    },
+    {
+        name: "Restaurant 16",
+        stars: 4,
+        score: 90,
+        cuisine: ["Chinese", "Japanese", "Indian"]
+    },
+    {
+        name: "Restaurant 17",
+        stars: 4,
+        score: 100,
+        cuisine: ["Indian", "Japanese", "Chinese"]
+    },
+    {
+        name: "Restaurant 18",
+        stars: 5,
+        score: 90,
+        cuisine: ["Japanese", "Chinese", "Indian"]
+    },
+    {
+        name: "Restaurant 19",
+        stars: 3,
+        score: 70,
+        cuisine: ["Chinese", "Indian"]
+    },
+    {
+        name: "Restaurant 20",
+        stars: 4,
+        score: 80,
+        cuisine: ["Indian", "Chinese"]
+    }
+];
